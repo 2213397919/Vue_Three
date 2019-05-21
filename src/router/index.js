2bc67f81d42c5b0705b423ccd2d8b2f2110e6db9
zhangshun <2213397918@qq.com>
2019-05-21 10:18:40 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Test from '@/views/Test'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/test',
       name: 'Test',
